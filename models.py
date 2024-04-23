ddbcfe3de7b38367e95dcfae25098b4ed3a1ba07
danxhuan <danxhuan@mail.ru>
2024-04-24 01:10:03 +0300

    Update models.py

--- models.py
+++ models.py
@@ -13,21 +13,14 @@ class User(db.Model):
 
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    country = db.Column(db.String(100), nullable=False)
-    city = db.Column(db.String(100), nullable=False)
+    start_country = db.Column(db.String(100), nullable=False)
+    finish_country = db.Column(db.String(100), nullable=False)
+    start_city = db.Column(db.String(100), nullable=False)
+    finish_city = db.Column(db.String(100), nullable=False)
     departure_date = db.Column(db.DateTime, nullable=False)
     arrival_date = db.Column(db.DateTime, nullable=False)
-    stay_duration = db.Column(db.Integer)
-    flight_duration = db.Column(db.Integer)
+    stay_time = db.Column(db.Integer)
+    flight_time = db.Column(db.Integer)
+    rating = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('trips', lazy=True))
-
-
-class Rating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    trip_id = db.Column(db.Integer, db.ForeignKey('trip.id'), nullable=False)
-    trip = db.relationship('Trip', backref=db.backref('ratings', lazy=True))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('ratings', lazy=True))
-    rating = db.Column(db.Integer, nullable=False)
-    rated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
